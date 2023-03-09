8bb8949079e0f660288c55f63e86b575fd8c9ef9
jezzixxx <pkapav02@gmail.com>
2023-03-09 14:58:07 +0300

    done work

---
+++ hello_world_application/_build/CMakeFiles/sources.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "sources"
+  "sources.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/sources.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ solver_application/formatter_ex/CMakeFiles/formatter_ex.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jezzixxx/projects/lab03/formatter_ex_lib/formatter_ex.cpp" "/home/jezzixxx/projects/lab03/solver_application/formatter_ex/CMakeFiles/formatter_ex.dir/formatter_ex.cpp.o" "gcc" "/home/jezzixxx/projects/lab03/solver_application/formatter_ex/CMakeFiles/formatter_ex.dir/formatter_ex.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jezzixxx/projects/lab03/solver_application/formatter/CMakeFiles/formatter.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
